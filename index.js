aba9e5bc16fc75cd8fb5492da528109b38b6c26e
larry <381582176@qq.com>
2021-11-01 15:04:26 +0800

    fix: 添加 run 的 hook

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const path = require('path')
 
 class TaroPreactWebpackPlugin {
-    apply(compiler) {
-      compiler.hooks.watchRun.tapAsync('TaroPreactWebpackPlugin', (compilation, callback) => {
-        // Explore each chunk (build output):
-        // compiler.options
-        Object.assign(compiler.options.resolve.alias, {
-          'nervjs': path.resolve(__dirname, 'nervjs-hack.js'),
-          'react': path.resolve(__dirname, 'nervjs-hack.js'),
-          'react-dom': path.resolve(__dirname, 'nervjs-hack.js'),
-        })
-  
-        callback();
-      });
+  apply(compiler) {
+    const modifier = (compilation, callback) => {
+      Object.assign(compiler.options.resolve.alias, {
+        'nervjs': path.resolve(__dirname, 'nervjs-hack.js'),
+        'react': path.resolve(__dirname, 'nervjs-hack.js'),
+        'react-dom': path.resolve(__dirname, 'nervjs-hack.js'),
+      })
+
+      callback();
     }
+    compiler.hooks.watchRun.tapAsync('TaroPreactWebpackPlugin',modifier);
+    compiler.hooks.run.tapAsync('TaroPreactWebpackPlugin',modifier);
   }
-  module.exports = TaroPreactWebpackPlugin;
+}
+module.exports = TaroPreactWebpackPlugin;
